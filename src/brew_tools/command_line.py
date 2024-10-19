8dab76474c2c0bfc2b24cb47a0a557f24f941aa7
Sven Steinbauer <sven@unlogic.co.uk>
2024-10-19 09:18:09 +0100

    Update version to 0.6.1

--- src/brew_tools/command_line.py
+++ src/brew_tools/command_line.py
@@ -3,7 +3,7 @@ import click
 import sys
 import logging
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 from brew_tools import inputs
 from brew_tools import config
 import brew_tools.brew_maths as bm
